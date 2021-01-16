290aaf40ab89e9ab3d793504f42691c367596adc
sebastiantroncoso <sctroncoso10@gmail.com>
2021-01-16 13:52:00 -0500

    Working on front end development

--- src/App.js
+++ src/App.js
@@ -1,25 +1,221 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ContactTable from "./components/ContactTable";
+import ContactForm from "./components/ContactForm";
+import ContactModal from "./components/ContactModal";
+import {Container, Row, Col} from 'react-bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SERVICE_URL = "https://tsg-contactlist.herokuapp.com"
+
+class App extends React.Component {
+    //  Allow the contactModal child component to pass back up the information via the eventHandler, where the parent
+    // App component can update its editContactData state variable, triggering a re-render of information displayed
+    // within the ContactModal form.
+    handleEditFormChange = (event) => {
+        let inputName = event.target.name;
+        let inputValue = event.target.value;
+        let contactInfo = this.state.editContactData;
+
+        console.log(`Something changed in ${inputName} : ${inputValue}`)
+
+        if(contactInfo.hasOwnProperty(inputName)) {
+            contactInfo[inputName] = inputValue;
+            this.setState({editContactData : contactInfo})
+        }
+    }
+
+
+    // Submits the collected data to our contact list REST service (PUT) and passes the data payload along inside the
+    // request body.
+
+    handleEditFormSubmit = (event) => {
+        if (event) event.preventDefault();
+        let contactId = event.target.value;
+        console.log(`Submitting edit for contact id ${contactId}`)
+        console.log(this.state.editContactData)
+
+        fetch(SERVICE_URL + '/contact/' + contactId, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state.editContactData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                this.setState({showEditModal: false})
+                this.loadContactData();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+
+    // Grabs the expected contactId form the triggered button and use that information to submit a request to our delete
+    // contact list REST service endpoint using that ID (DELETE to /contact/{id}. If successful, the resulting success
+    // promise should trigger a reload of the ContactTable by calling loadContactDAta to reload the contactData state
+    // variable within the App.
+    handleDeleteContact = (event) => {
+        if (event) event.preventDefault();
+        let contactId = event.target.value;
+        console.log(`Submitting delete for contact id ${contactId}`)
+
+        fetch(SERVICE_URL + '/contact/' + contactId, {
+            method: 'DELETE',
+        })
+            .then(data => {
+                this.loadContactData();
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }
+
+    handleEditModalClose = (event) => {
+        console.log("Closing Edit Modal")
+        this.setState({showEditModal: false})
+    }
+
+    handleEditModalOpen = (event) => {
+        console.log("Opening Edit Modal")
+        if (event) event.preventDefault();
+        let contactId = event.target.value;
+        console.log(`Editing contact id ${contactId}`)
+        this.setState({showEditModal: true})
+
+        // submit a GET request to the /contact/{contactId} endpoint
+        // the response should come back with the associated contact's JSON
+        fetch(SERVICE_URL + '/contact/' + contactId)
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                this.setState(
+                    {editContactData: data, showEditModal: true}
+                )
+            })
+            .catch((error) => {
+                console.error('Error', error);
+            })
+    }
+
+    handleAddFormChange = (event) => {
+        let inputName = event.target.name;
+        let inputValue = event.target.value;
+        let contactInfo = this.state.newContactData;
+
+        console.log(`Updating new contact data: ${inputName}: ${inputValue}`)
+
+        if(contactInfo.hasOwnProperty(inputName)){
+            contactInfo[inputName] = inputValue;
+            this.setState({newContactData: contactInfo})
+        }
+    }
+
+    handleAddFormSubmit = (event) => {
+        console.log("Adding contact!")
+        if (event) event.preventDefault();
+
+        fetch(SERVICE_URL + '/contact/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state.newContactData),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Add Contact - Success:', data);
+                this.setState({ newContactData: { firstName: '', lastName: '', company: '', phone: '', email: '' } })
+                this.loadContactData();
+            })
+            .catch((error) => {
+                console.log('Add Contact - Error:')
+                console.log(error)
+            });
+    }
+
+
+    loadContactData() {
+        this.setState({ loading: true })
+        console.log("Loading contact data")
+        fetch(SERVICE_URL + "/contacts")
+            .then(data => data.json())
+            .then(data => this.setState(
+                { contactData: data, loading: false }
+            ))
+    }
+
+    state = {
+        loading: false,
+        showEditModal: false,
+        contactData: [
+            {
+                "contactId": 1, "firstName": "Fake",
+                "lastName": "Data",
+                "company": "Unknown Inc.",
+                "phone": "000-0000",
+                "email": "fakedata@unknown.io"
+            }],
+        newContactData: {
+            firstName: '',
+            lastName: '',
+            company: '',
+            phone: '',
+            email: ''
+        },
+        editContactData: {
+            "contactId": 42,
+            "firstName" : "Zaphod",
+            "lastName": "Beeblebrox",
+            "company": "Heart of Gold",
+            "phone": "000-0000",
+            "email": "prez@badnews.us"
+        }
+    }
+
+    componentDidMount() {
+        console.log("App is now mounted.")
+        this.loadContactData();
+    }
+
+    render() {
+        return (
+            <Container fluid>
+                <Row>
+                    <Col>
+                        <h1 className="text-center">Contact Application</h1>
+                    </Col>
+                </Row>
+                <hr/>
+                <Row>
+                    <Col sm={8}>
+                        <h2>My Contacts</h2>
+                        <ContactTable
+                            contacts={this.state.contactData}
+                            handleEdit={this.handleEditModalOpen}
+                            handleDelete={this.handleDeleteContact}
+                        />
+                    </Col>
+                    <Col sm={4}>
+                        <h2>Add New Contact</h2>
+                        <ContactForm
+                            handleSubmit={this.handleAddFormSubmit}
+                            handleChange={this.handleAddFormChange}
+                            contactData={this.state.newContactData} />
+                    </Col>
+                </Row>
+                <ContactModal
+                    show={this.state.showEditModal}
+                    handleSubmit={this.handleAddFormSubmit}
+                    handleChange={this.handleAddFormChange}
+                    handleClose={this.handleEditModalClose}
+                    contactData={this.state.editContactData}/>
+            </Container>
+        );
+    }
 }
 
 export default App;
